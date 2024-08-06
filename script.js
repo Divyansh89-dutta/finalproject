ced0ae8d9cd3a3d7cc6b5888641e223319ac05db
Divyansh89-dutta <divyanshduttaroy163@gmail.com>
2024-08-06 14:51:52 +0530

    page5 card

--- script.js
+++ script.js
@@ -1,126 +1,131 @@
-function locomotiveSetup() {
-    gsap.registerPlugin(ScrollTrigger);
-    const locoScroll = new LocomotiveScroll({
-        el: document.querySelector("#main"),
-        smooth: true,
-    });
-    locoScroll.on("scroll", ScrollTrigger.update);
-    ScrollTrigger.scrollerProxy("#main", {
-        scrollTop(value) {
-            return arguments.length
-                ? locoScroll.scrollTo(value, 0, 0)
-                : locoScroll.scroll.instance.scroll.y;
-        },
-        getBoundingClientRect() {
-            return {
-                top: 0,
-                left: 0,
-                width: window.innerWidth,
-                height: window.innerHeight,
-            };
-        },
-        pinType: document.querySelector("#main").style.transform
-            ? "transform"
-            : "fixed",
-    });
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-    ScrollTrigger.refresh();
+
+function smoothScrolling() {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
 }
-// locomotiveSetup();
-function formDivAppearance(){
-    var form = document.querySelector("form")
-    var formDiv = document.querySelector("#form-div")
-    var main = document.querySelector("#main")
-    var input = document.querySelector("form input")
-    form.addEventListener("submit", function (e) {
-        e.preventDefault()
-        var user = input.value
-        localStorage.setItem('username', user)
-        formDiv.style.display = "none"
-        main.style.display = "initial"
-    })
-    if(localStorage.length>0){
-        formDiv.style.display = "none"
-        main.style.display = "initial"
-    }
+
+smoothScrolling()
+
+function formDivAppearance() {
+  var formDiv = document.querySelector("#form-div");
+  var form = document.querySelector("form");
+  var main = document.querySelector("#main");
+  var input = document.querySelector("form input");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var user = input.value;
+    localStorage.setItem("username", user);
+    formDiv.style.display = "none";
+    main.style.display = "initial";
+  });
+
+  if (localStorage.length > 0) {
+    formDiv.style.display = "none";
+    main.style.display = "initial";
+  }
 }
+
 formDivAppearance();
 
-function page1Loading(){
-    let tl = gsap.timeline();
-    tl.from("#page1-hero img", {
-        y: 50,
-        opacity: 0.6,
-        stagger: 0.2,
-        delay: 0.3,
-    });
-    tl.from("#page1-hero h3",{
-        y: 20,
-        opacity: 0,
-        duration: 0.3,
-        stagger: 0.2,
-    }, "-=0.6");
-    tl.from(
-        "#bg-img1",
-        {
-          x: -100,
-          duration: 0.4,
-        },
-        "-=0.6"
-      );
-      tl.from(
-        "#bg-img2",
-        {
-          x: 100,
-          duration: 0.4,
-        },
-        "-=0.6"
-      );
+function page1Loading() {
+  let tl = gsap.timeline();
+
+  tl.from("#page1-hero img", {
+    y: 50,
+    opacity: 0,
+    duration: 0.6,
+    stagger: 0.2,
+    delay: 0.3,
+  });
+  tl.from(
+    "#page1-hero h3",
+    {
+      y: 20,
+      opacity: 0,
+      duration: 0.3,
+      stagger: 0.2,
+    },
+    "-=0.6"
+  );
+  tl.from(
+    "#bg-img1",
+    {
+      x: -100,
+      duration: 0.4,
+    },
+    "-=0.6"
+  );
+  tl.from(
+    "#bg-img2",
+    {
+      x: 100,
+      duration: 0.4,
+    },
+    "-=0.6"
+  );
 }
-var growth = document.querySelector(".loader-growth");
-var loader = document.querySelector("#loader");
-var grow = 0;
+function loadingAnimation() {
+  var growth = document.querySelector(".loader-growth");
+  var loader = document.querySelector("#loader");
+  var grow = 0;
 
-var loaderInterval = setInterval(function () {
-  grow++;
+  var loaderInterval = setInterval(function () {
+    grow++;
 
-  growth.style.width = grow + "%";
-}, 18);
+    growth.style.width = grow + "%";
+  }, 18);
 
-gsap.to("#loader img", {
-  rotate: 60,
-  duration: 10,
-});
+  gsap.to("#loader img", {
+    rotate: 60,
+    duration: 10,
+  });
 
-let tl = gsap.timeline()
+  let tl = gsap.timeline();
 
-tl.to("#loader img",{
-  delay:2.3,
-  opacity:0
-})
-tl.to(".loader-text",{
-  opacity:0
-})
-tl.to("#loader",{
-  top:"-100%"
-})
+  tl.to("#loader img", {
+    delay: 2.3,
+    opacity: 0,
+  });
+  tl.to(".loader-text", {
+    opacity: 0,
+  });
+  tl.to("#loader", {
+    top: "-100%",
+  });
 
-setTimeout(function () {
-  clearInterval(loaderInterval);
+  setTimeout(function () {
+    clearInterval(loaderInterval);
 
-  page1Loading();
-}, 3100);
+    page1Loading();
+  }, 3100);
+}
+loadingAnimation()
+
 function page2Animation() {
-  gsap.to("#page2", {
-    backgroundColor: "#388699",
+  var tl2 = gsap.timeline({
     scrollTrigger: {
       trigger: "#page2",
       scroller: "body",
-      start: "top 0%",
-      end: "top -20%",
+      start: "top -5%",
+      end: "top -120%",
       scrub: 2,
     },
   });
+
+  tl2.to("#page2, #page3", {
+    backgroundColor: "#388699",
+  });
+  tl2.to("#page2, #page3", {
+    backgroundColor: "#D6E2E5",
+  });
+
   var page2h2 = document.querySelector("#page2-content-text h2").textContent;
 
   var splittedText = page2h2.split(" ");
@@ -158,50 +163,72 @@ function page2Animation() {
   });
 }
 
-page2Animation()
+page2Animation();
 
-gsap.to("#page3 .page3-side-content",{
-  y:-420,
-  scrollTrigger:{
-    trigger:"#page3",
-    scroller:"body",
-    start:"top -25%",
-    end:"top -40%",
-    scrub:2,
-    pin:true
-  }
-})
-var vr = gsap.timeline({
-  scrollTrigger:{
-    trigger:"#page4",
-    scroller:"body",
-    start:"top 0",
-    end:"top -100%",
-    scrub:1,
-    pin:true
-  }
-})
+function page3and4Animation() {
+  gsap.to("#page3 .page3-side-content", {
+    y: -340,
+    scrollTrigger: {
+      trigger: "#page3",
+      scroller: "body",
+      start: "top -15%",
+      end: "top -60%",
+      scrub: 2,
+      pin: true,
+    },
+  });
 
-vr.to("#page4-content-inner",{
-  y:'-7vw',
-  // opacity:0,
-  duration:1,
-  delay:0.5
-},'anim')
-vr.from("#h1two",{
-  opacity:0,
-  duration:1,
-  delay:0.5
-},'anim')
-vr.to("#page4-content-inner",{
-  y:'-14.5vw',
-  // opacity:0,
-  duration:1,
-  delay:0.5
-},'anim2')
-vr.from("#h1three",{
-  opacity:0,
-  duration:1,
-  delay:0.5
-},'anim2')
-// localStorage. clear()
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#page4",
+      scroller: "body",
+      start: "top 0",
+      end: "top -100%",
+      scrub: 1,
+      pin: true,
+    },
+  });
+
+  tl.to(
+    "#page4-content-inner",
+    {
+      y: "-7vw",
+      duration: 1,
+      delay: 0.5,
+    },
+    "anim"
+  );
+  tl.from(
+    "#h1two",
+    {
+      opacity: 0,
+      duration: 1,
+      delay: 0.5,
+    },
+    "anim"
+  );
+  tl.to(
+    "#page4-content-inner",
+    {
+      y: "-14.5vw",
+      duration: 1,
+      delay: 0.5,
+    },
+    "anim2"
+  );
+  tl.from(
+    "#h1three",
+    {
+      opacity: 0,
+      duration: 1,
+      delay: 0.5,
+    },
+    "anim2"
+  );
+}
+
+page3and4Animation();
+
+
+
+document.querySelector("#certificate-content h1").innerHTML = localStorage.getItem('username')
